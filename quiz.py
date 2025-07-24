efc7a8a4476389b8fc8b4212de6883c723fc3639
Zirnoix <juscor07@gmail.com>
2025-07-24 18:25:45 +0200

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,12 @@
+# Fichier : quiz.py
+# Commandes : animequiz, animequizmulti, animebattle, duel, quiztop, myrank
+
+from discord.ext import commands
+import discord
+
+# Dépendances locales
+from modules.utils import *
+
+def setup(bot):
+    # Les commandes seront ajoutées ici
+    pass
