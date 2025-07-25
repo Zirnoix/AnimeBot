0867465f193b22c092c846f86fc780efe42f2f3a
Zirnoix <juscor07@gmail.com>
2025-07-25 16:57:01 +0200

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,10 +1,26 @@
-from helpers.json_utils import load_json, save_json
-from helpers.anilist_utils import normalize_title, get_anilist_user_animelist
-from helpers.stats_utils import generate_genre_chart, generate_stats_embed
+# Imports des helpers
+from helpers.json_utils import (
+    load_json,
+    save_json,
+)
+
 from helpers.general_utils import (
-    get_user_anilist, genre_emoji, get_user_stats,
-    get_user_genres, get_user_genre_chart,
-    get_all_user_genres, get_upcoming_episodes,
-    search_anime, get_top_animes, get_seasonal_animes,
-    TIMEZONE
+    normalize_title,
+    genre_emoji,
+    TIMEZONE,
+)
+
+from helpers.anilist_utils import (
+    get_user_anilist,
+    get_upcoming_episodes,
+    search_anime,
+    get_top_animes,
+    get_seasonal_animes,
+)
+
+from helpers.stats_utils import (
+    get_user_stats,
+    get_user_genres,
+    get_user_genre_chart,
+    get_all_user_genres,
 )
