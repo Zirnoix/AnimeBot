a1d0f4e0be24b300e01f01db807e1594c72518c0
Zirnoix <juscor07@gmail.com>
2025-07-25 17:00:48 +0200

    Update anilist_utils.py

--- helpers/anilist_utils.py
+++ helpers/anilist_utils.py
@@ -5,7 +5,27 @@ import re
 
 ANILIST_API = "https://graphql.anilist.co"
 OWNER_USERNAME = os.getenv("ANILIST_USERNAME")
+ANILIST_USERNAME = os.getenv("ANILIST_USERNAME")
 
+def get_user_anilist(username: str = None):
+    if username is None:
+        username = ANILIST_USERNAME
+
+    query = """
+    query ($name: String) {
+        User(name: $name) {
+            id
+            name
+        }
+    }
+    """
+    variables = {"name": username}
+    response = requests.post("https://graphql.anilist.co", json={"query": query, "variables": variables})
+
+    if response.status_code != 200:
+        raise Exception("Erreur lors de la récupération de l'utilisateur AniList")
+
+    return response.json()["data"]["User"]p
 def normalize_title(title):
     nfkd = unicodedata.normalize('NFKD', title)
     cleaned = "".join(c for c in nfkd if not unicodedata.combining(c))
