50223f44980a7e502aee897ca3a2addd0ee9bc5e
Zirnoix <juscor07@gmail.com>
2025-07-27 12:47:42 +0200

    Delete modules directory

--- modules/commands/test.py
+++
@@ -1,9 +0,0 @@
-
-from discord.ext import commands
-
-@commands.command(name="ping")
-async def ping(ctx):
-    await ctx.send("🏓 Pong ! Le bot est en ligne.")
-
-def setup(bot):
-    bot.add_command(ping)
--- modules/title_cache.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def get_random_anime_title():
-    sample_titles = ["Naruto", "One Piece", "Bleach", "My Hero Academia"]
-    return random.choice(sample_titles)
