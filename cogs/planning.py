ac467f0a80face63caffc03eb625845d46364882
Zirnoix <juscor07@gmail.com>
2025-07-27 19:11:54 +0200

    Update planning.py

--- cogs/planning.py
+++ cogs/planning.py
@@ -1,1 +1,20 @@
-# Planning Cog complet ici
+import discord
+from discord.ext import commands
+from modules.anilist import get_personal_planning, get_seasonal_anime
+
+class Planning(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="monplanning")
+    async def monplanning(self, ctx):
+        embed = await get_personal_planning(ctx.author.id)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="seasonal")
+    async def seasonal(self, ctx):
+        embed = await get_seasonal_anime()
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Planning(bot))
