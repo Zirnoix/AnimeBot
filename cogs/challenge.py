a2b183607fab09071f6e0f61083cbf34751f63ef
Zirnoix <juscor07@gmail.com>
2025-07-27 19:05:27 +0200

    Update challenge.py

--- cogs/challenge.py
+++ cogs/challenge.py
@@ -1,1 +1,24 @@
-# Challenge Cog complet ici
+from discord.ext import commands
+from modules.anilist import get_challenge_data, get_weekly_challenge, complete_challenge
+
+class Challenge(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="anichallenge")
+    async def anichallenge(self, ctx):
+        embed = await get_challenge_data(ctx.author.id)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="weekly")
+    async def weekly(self, ctx):
+        embed = await get_weekly_challenge()
+        await ctx.send(embed=embed)
+
+    @commands.command(name="complete")
+    async def complete(self, ctx):
+        embed = await complete_challenge(ctx.author.id)
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Challenge(bot))
